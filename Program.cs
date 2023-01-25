ba3a2fdff0512b7d76e57d6d124822cd9e6040f7
wallaceCoelho <wacoelho.dev@gmail.com>
2023-01-24 23:56:27 -0300

    Desafio finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia1 = new Nokia("1234321", "Nokia A34", "1DEW2JE9KKE3", 128);
+
+Console.WriteLine(nokia1.Numero);
+nokia1.InstalarAplicativo("Facebook");
+nokia1.ReceberLigacao();
+nokia1.Ligar();
+
+Iphone iphone13 = new Iphone("5677658", "Iphone 13", "7VPO5JE9L6G3", 64);
+
+Console.WriteLine(nokia1.Numero);
+iphone13.InstalarAplicativo("Whatsapp");
+iphone13.ReceberLigacao();
+iphone13.Ligar();
